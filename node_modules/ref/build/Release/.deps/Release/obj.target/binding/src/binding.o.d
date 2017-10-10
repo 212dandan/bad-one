92b7443da02de191247cbca6aab2f28496fc1561
loongson <loongson@localhost.localdomain>
2017-10-10 19:00:23 +0800

    ffi library

---
+++ node_modules/ref/build/Release/.deps/Release/obj.target/binding/src/binding.o.d
@@ -0,0 +1,52 @@
+cmd_Release/obj.target/binding/src/binding.o := g++ '-DBUILDING_NODE_EXTENSION' -I/usr/include/node -I/usr/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/binding/src/binding.o.d.raw  -c -o Release/obj.target/binding/src/binding.o ../src/binding.cc
+Release/obj.target/binding/src/binding.o: ../src/binding.cc \
+ /usr/include/node/node.h /usr/include/node/v8.h \
+ /usr/include/node/v8-version.h /usr/include/node/v8config.h \
+ /usr/include/node/node_version.h /usr/include/node/node_buffer.h \
+ /usr/include/node/node.h ../node_modules/nan/nan.h \
+ /usr/include/node/node_version.h /usr/include/node/uv.h \
+ /usr/include/node/uv-errno.h /usr/include/node/uv-version.h \
+ /usr/include/node/uv-unix.h /usr/include/node/uv-threadpool.h \
+ /usr/include/node/uv-linux.h /usr/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h
+../src/binding.cc:
+/usr/include/node/node.h:
+/usr/include/node/v8.h:
+/usr/include/node/v8-version.h:
+/usr/include/node/v8config.h:
+/usr/include/node/node_version.h:
+/usr/include/node/node_buffer.h:
+/usr/include/node/node.h:
+../node_modules/nan/nan.h:
+/usr/include/node/node_version.h:
+/usr/include/node/uv.h:
+/usr/include/node/uv-errno.h:
+/usr/include/node/uv-version.h:
+/usr/include/node/uv-unix.h:
+/usr/include/node/uv-threadpool.h:
+/usr/include/node/uv-linux.h:
+/usr/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
